5936a786e218ac53ca65ee650a1d86f2e62d967f
austin1985 <tigerinformation00@gmail.com>
2019-01-27 21:16:19 +0100

    JWT authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb://austin:asdQWE123@ds213615.mlab.com:13615/connectingdevs'
+    mongoURI : 'mongodb://austin:asdQWE123@ds213615.mlab.com:13615/connectingdevs',
+    secretOrKey : 'secret'
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile')
@@ -7,25 +8,29 @@ const posts = require('./routes/api/posts')
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 //DB config
 const db = require('./config/keys').mongoURI;
 
 //Connect to mondgoDB
 mongoose
     .connect(db)
-    .then(()=>{
+    .then(() => {
         console.log('MongoDB connected');
     })
-    .catch(err =>{
+    .catch(err => {
         console.log(err);
     })
 
 
-app.get('/', (req,res) =>res.send('Server running!'));
+app.get('/', (req, res) => res.send('Server running!'));
 
-app.use('/api/users',users)
-app.use('/api/profile',profile)
-app.use('/api/posts',posts)
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000;
 
