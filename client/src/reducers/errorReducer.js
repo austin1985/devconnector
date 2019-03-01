ae114247faf77142d97040bce66c522be6e28a48
Mihaly Balogh <tigerinformation00@gmail.com>
2019-03-01 14:07:42 +0100

    WIP

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,5 +1,5 @@
 
-import {GET_ERRORS} from '../actions/types'
+import { GET_ERRORS } from '../actions/types'
 const initialState = {
 
 }
@@ -9,7 +9,7 @@ export default function (state = initialState, action) {
     switch (action.type) {
 
         case GET_ERRORS:
-        return action.payload
+            return action.payload
 
         default:
             return state;
