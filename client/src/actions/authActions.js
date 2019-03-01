e17e0a6d01f94976f73d82f3f8ccac79190907b2
Mihaly Balogh <tigerinformation00@gmail.com>
2019-03-01 20:23:36 +0100

    WIP

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -50,3 +50,14 @@ export const setCurrentUser = (decoded) => {
         payload: decoded
     }
 }
+
+
+// Log user out
+export const logoutUser = () => dispatch => {
+    //Remove token from localstorage
+    localStorage.removeItem('jwtToken')
+    //Remove auth header for future requests
+    setAuthToken(false)
+    // Set current user to {} wich will set isAuthenticated to false
+    dispatch(setCurrentUser({}))
+}
