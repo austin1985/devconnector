9ff44dc304f12b94518c1f611965d03fa6bc4b9e
Mihaly Balogh <tigerinformation00@gmail.com>
2019-03-05 16:11:07 +0100

    Devconnector add profile frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import  jwt_decode  from 'jwt-decode'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
-import {setCurrentUser, logoutUser} from './actions/authActions'
+import { setCurrentUser, logoutUser } from './actions/authActions'
+import { clearCurrentProfile } from './actions/profileActions';
+
 import { Provider } from 'react-redux'
 import store from './store'
+
+import PrivateRoute from './components/common/PrivateRoute'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Foter'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
 
 import './App.css';
 
@@ -29,9 +35,10 @@ if (localStorage.jwtToken) {
 	if (decoded.exp < currentTime) {
 		// Logout user
 		store.dispatch(logoutUser())
-		// TODo: Clear current profile
+		// Clear current profile
+		store.dispatch(clearCurrentProfile())
 		// redirect to login
-		
+
 		window.location.href = '/login'
 	}
 }
@@ -44,11 +51,15 @@ class App extends Component {
 					<div className="App">
 						<Navbar />
 						<Route exact path="/" component={Landing} />
-						<div>
+						<div className="container">
 							<Route exact path="/register" component={Register} />
-						</div>
-						<div>
 							<Route exact path="/login" component={Login} />
+							<Switch>
+								<PrivateRoute exact path="/dashboard" component={Dashboard} />
+							</Switch>
+							<Switch>
+								<PrivateRoute exact path="/create-profile" component={CreateProfile} />
+							</Switch>
 						</div>
 						<Footer />
 					</div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { logoutUser } from '../../actions/authActions'
+import {clearCurrentProfile} from '../../actions/profileActions'
+
 
 class Navbar extends Component {
 	onLogoutClick(e) {
 		e.preventDefault();
+		this.props.clearCurrentProfile()
 		this.props.logoutUser()
 	}
 
@@ -70,6 +73,7 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
+	clearCurrentProfile: PropTypes.func.isRequired,
 	logoutUser: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired
 }
@@ -78,4 +82,4 @@ const mapStateToProps = (state) => ({
 	auth: state.auth
 })
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
