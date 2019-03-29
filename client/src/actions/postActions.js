036926dc53d835764287dae3850586260ed0bfd0
Mihaly Balogh <tigerinformation00@gmail.com>
2019-03-29 10:36:50 +0100

    posts wip

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,7 +3,8 @@ import {
     ADD_POST,
     GET_ERRORS,
     GET_POSTS,
-    POST_LOADING
+    POST_LOADING,
+    DELETE_POST
 } from './types'
 
 
@@ -38,6 +39,51 @@ export const getPosts = () => dispatch => {
             dispatch({
                 type: GET_POSTS,
                 payload: null
+            })
+        })
+}
+
+// Delete post
+export const deletePost = id => dispatch => {
+    axios.delete(`/api/posts/${id}`)
+        .then(res => {
+            dispatch({
+                type: DELETE_POST,
+                payload: id
+            })
+        })
+        .catch(error => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: error.response.data
+            })
+        })
+}
+
+// Add like
+export const addLike = id => dispatch => {
+    axios.post(`/api/posts/like/${id}`)
+        .then(res => {
+            dispatch(getPosts())
+        })
+        .catch(error => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: error.response.data
+            })
+        })
+}
+
+// Remove like
+export const removeLike = id => dispatch => {
+    axios.post(`/api/posts/unlike/${id}`)
+        .then(res => {
+            dispatch(getPosts())
+        })
+        .catch(error => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: error.response.data
             })
         })
 }
